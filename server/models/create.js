23ef55cc1605f873b559ac8238f53772272d1f68
Tanmaysingh2210 <tanmayrajpoot2210@gmail.com>
2025-03-27 12:19:43 +0530

    created

---
+++ server/models/create.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const createSchema = new Schema({
+  task: String,
+  isDone: Boolean,
+  deadline: Date
+  
+},
+{
+    timestamps:true
+});
+
+  const Create = mongoose.model('Create', createSchema);
+module.exports = Create
+
