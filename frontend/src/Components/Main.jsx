549bdb1ed93c7723939e35668c539c81a7d7a086
Ayush vashisth <ayushvashisth@Blanche.local>
2025-03-28 13:24:12 +0530

    updated

--- frontend/src/Components/Main.jsx
+++ frontend/src/Components/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Main.css'
 import svg from '../assets/add.svg'
+import tasklist from './tasklist.jsx';
 
 const Main = () => {
   return (
@@ -9,10 +10,7 @@ const Main = () => {
       <button  className='edit'>Edit</button>
 
       <div className='box'>
-        <div className='bars'>
-        Watch movie
 
-        </div>
       </div>
 
 
---
+++ frontend/src/Components/tasklist.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const tasklist = () => {
+  return (
+    <div className='bars'>
+    Watch movie
+    </div>
+  )
+}
+
+export default tasklist
