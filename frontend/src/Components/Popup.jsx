dae7bd7bf59f3cf29ce0e774d0d65e3faf4c65a7
shlok-bhatia <shlokbhatia16@gmail.com>
2025-03-28 11:05:33 +0530

    xasib

--- frontend/src/Components/Popup.jsx
+++ frontend/src/Components/Popup.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Popup.css'
 
 const Popup = () => {
+
+  
+const [username, setusername] = useState('');
+
+  const submitHandler=(e)=>{
+    e.preventDefault()
+    console.log(username);
+    setusername('')
+      
+  }
+
   return (
     <div className='popup'>
+      <h1 className='text-[#6B01BC] text-2xl font-semibold '>ADD TASK</h1>
+      <form onSubmit={(e)=>[
+        submitHandler(e)
+      ]} >
+        <input value={username}
+        onChange={(e)=>{          
+          setusername(e.target.value);          
+        }} 
+         type="text" placeholder='Enter your task' className='bg-white px-4 py-4 text-xl m-5 rounded'/>
+        <button  className='bg-emerald-600 rounded px-4 py-3 font-semibold text-xl m-5'>Add task</button>
+
+      </form>
       
     </div>
   )
