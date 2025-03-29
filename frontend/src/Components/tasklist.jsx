ce97b268f7537183799e9162611d71d200cb4b56
shlok-bhatia <shlokbhatia16@gmail.com>
2025-03-29 13:10:48 +0530

    uhw

--- frontend/src/Components/tasklist.jsx
+++ frontend/src/Components/tasklist.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const Tasklist = (props) => {
   return (
     <div className='task'>
     {props.taskName}
+    <EditIcon  style={{ fontSize: 30, color: 'white', marginLeft:'8em'}}/>
+    <DeleteIcon  style={{ fontSize: 30, color: 'white' }}/>
     </div>
   )
 }
